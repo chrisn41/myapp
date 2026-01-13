e461f546af6296f15da4a906bc3cc592693a5ff2
chris <chris@chrisn-laptop.>
2026-01-13 16:43:56 +0700

    test cicd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello DevOps 🚀 App is running');
+  res.send('CI/CD SUCCESS 🚀 Deploy otomatis jalan');
 });
 
 app.listen(PORT, () => {
